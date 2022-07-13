31e2e34f8fd021543b7c5c54ec4bbb408ea84530
Alexander Williams <alex.williams2311@gmail.com>
2022-07-13 14:00:01 -0700

    Initial Commit

---
+++ MathHeadz/MathHeadzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathHeadzApp.swift
+//  MathHeadz
+//
+//  Created by alexander williams on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MathHeadzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
